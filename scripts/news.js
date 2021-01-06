08a3a6a9d5ab62f7356eac71637a87858e32b1ce
Jared Kelly <jared.kelly@dominos.com.au>
2021-01-06 10:40:37 +1000

    added news and second subreddit

--- scripts/news.js
+++ scripts/news.js
@@ -1,21 +1,23 @@
 const num_headlines = 10;
 const max_chars = 90;
 
-let news_content = "<h2>/r/worldnews</h2><ul>";
-let news_request = new XMLHttpRequest();
-news_request.open("GET", "https://www.reddit.com/r/worldnews/.json");
-news_request.send();
-news_request.onload = () => {
-    news_items = JSON.parse(news_request.response).data.children;
-    console.log(news_items);
-    for (let i=0; i < num_headlines; i++) {
-    // news_items.forEach(item => {
-        item = news_items[i]
-        news_content += ("<li><a href=" + item.data.url + "> - " + truncate(item.data.title) + "</li>");
-        console.log(item.data.title)
-    };
-    news_content += "</ul>"
-    document.getElementById('news').innerHTML = news_content;
+const getRedditContent = (subreddit, divName) => {
+    let news_content = `<h2>/r/${subreddit}</h2><ul>`;
+    let news_request = new XMLHttpRequest();
+    news_request.open("GET", `https://www.reddit.com/r/${subreddit}/.json`);
+    news_request.send();
+    news_request.onload = () => {
+        news_items = JSON.parse(news_request.response).data.children;
+        console.log(news_items);
+        for (let i=0; i < num_headlines; i++) {
+        // news_items.forEach(item => {
+            item = news_items[i]
+            news_content += (`<li><a href=https://old.reddit.com${item.data.permalink}>💬</a> <a href=${item.data.url}>${truncate(item.data.title)}</a></li>`);
+            console.log(item.data.title)
+        };
+        news_content += "</ul>"
+        document.getElementById(divName).innerHTML = news_content;
+    }
 }
 
 const truncate = (text) => {
@@ -24,3 +26,6 @@ const truncate = (text) => {
     }
     return text.substring(0,max_chars-3) + "..."
 }
+
+getRedditContent('worldnews', 'news')
+getRedditContent('australia', 'aus')
